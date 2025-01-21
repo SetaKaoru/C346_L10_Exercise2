7925254b9594c33c4fc32627e5238d2f122214a4
SetaKaoru <rhysdarys@gmail.com>
2025-01-21 14:26:12 +0800

    lol

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, StyleSheet} from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
 
-export default function App() {
+let originalData = []
+
+const styles = StyleSheet.create({
+    box: {
+        flexDirection: 'column',
+        backgroundColor: 'moccasin',
+        padding:'auto',
+        marginTop: '7',
+        marginLeft: 5,
+        marginRight: 5,
+        borderWidth: 1
+    },
+    header:{
+        textAlign:'center',
+        fontWeight:'600',
+        fontSize: 26,
+        marginBottom: 10,
+        backgroundColor: 'lightblue',
+    },
+    titleStyle: {
+        fontSize: 20,
+        marginLeft: 7,
+        fontWeight: '500'
+    },
+    textStyle: {
+        fontSize: 15,
+        marginLeft: 7,
+    },
+});
+
+const App = () => {
+  const [mydata, setMyData] = useState([]);
+
+  // Exercise 1B
+    useEffect(() => {
+        // Exercise 1A
+        fetch("https://mysafeinfo.com/api/data?list=titanic&format=json&case=default").then((response) => {
+            return response.json();
+        }).then((myJson)=>{
+            if(originalData.length < 1){
+                setMyData(myJson);
+                originalData = myJson;
+            }
+            setMyData(myJson);
+        })
+    }, []);
+
+    const FilterData = (text) => {
+        if (text != ' '){
+            let myFilteredData = originalData.filter((item) =>
+            item.PassengerName.includes(text));
+            setMyData(myFilteredData);
+        }
+        else{
+            setMyData(originalData);
+        }
+    }
+
+
+
+  const renderItem = ({item, index}) => {
+    return (
+    <View>
+        <View style={styles.box}>
+            <Text style={[styles.titleStyle, {paddingTop: 3}]}>{item.PassengerName}</Text>
+            <Text style={styles.textStyle}>Age: {item.Age}</Text>
+            <Text style={styles.textStyle}>Passenger Type: {item.PassengerType}</Text>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.textStyle}>Status:</Text>
+                <Text style={[styles.textStyle, {fontWeight: 700, paddingBottom: 3}]}>{item.Status}</Text>
+            </View>
+        </View>
+    </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{backgroundColor: 'mintcream'}}>
+      <StatusBar/>
+        <Text style={styles.header}>Titanic Passengers & Crew <Icon name="ship" size={26} color="black" /></Text>
+      <Text style={{marginLeft: 5, fontSize: 18, fontWeight: 500, }}>Search (Status):</Text>
+      <TextInput style={{borderWidth:1, marginLeft: 5, marginRight: 5, backgroundColor: 'white', marginBottom: 10,}} onChangeText={(text)=>{FilterData(text)}}/>
+      <FlatList data={mydata} renderItem={renderItem}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
